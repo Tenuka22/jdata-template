ad098f0e07e33fe3cadeccb1474e517fdc80eda4
Tenuka Omaljith <tenukaomaljith2009@gmail.com>
2024-06-08 15:05:11 +0530

    bug-fixes

--- components/json-editor.tsx
+++ components/json-editor.tsx
@@ -21,24 +21,7 @@ const JsonEditor = ({
 
   return (
     <div className="w-full h-full flex-1 flex-grow pb-4">
-      <div className="dark:hidden flex h-full w-full">
-        <Editor
-          options={{
-            minimap: {
-              enabled: false,
-            },
-          }}
-          className={cn("md:min-w-80 flex-1 w-full h-full min-h-80", className)}
-          height="10rem"
-          theme="light"
-          language={"json"}
-          defaultValue={jsonText}
-          onMount={onMount}
-          value={jsonText}
-          onChange={(newValue) => setJson(newValue || "")}
-        />
-      </div>
-      <div className="hidden dark:flex h-full w-full">
+      <div className=" h-full w-full">
         {" "}
         <Editor
           options={{
